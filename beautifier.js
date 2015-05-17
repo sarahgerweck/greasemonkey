46b69cbb3438a90e7588b6999108a81e6d99bb24
Sarah Gerweck <sarah.a180@gmail.com>
2015-05-16 19:13:19 -0700

    Initial version of HPL beautifier

---
+++ beautifier.js
@@ -0,0 +1,55 @@
+// ==UserScript==
+// @name        LovecraftEmDashes
+// @namespace   http://gerweck.org/
+// @include     http://www.dagonbytes.com/thelibrary/lovecraft/*.htm
+// @version     1
+// @grant       none
+// @require     http://osteele.com/sources/javascript/functional/functional.min.js
+// ==/UserScript==
+
+var Beautifier = (function() {
+  var exports = {};
+  var regexes = [
+    [/([^\s'])'([^']|$)/g, '$1’$2'],
+    [/(^|\s)'(\S)/g,       '$1‘$2'],
+    [/(^|\s)"([^\s"])/g,   '$1“$2'],
+    [/([^\s"])"(\s|$)/g,   '$1”$2'],
+    [/(\d)-(\d)/g,         '$1–$2'],
+    [/\s+-\s+|--| -- /g,   '—'],
+    [/\Q...\E/g,           '…'],
+  ];
+  exports.apply = function(inputString) {
+    var workingValue = inputString;
+    for (var i = 0; i < regexes.length; i++) {
+      workingValue = workingValue.replace(regexes[i][0], regexes[i][1]);
+    }
+    return workingValue;
+  };
+  return exports;
+})();
+
+var TextMapper = function(xpath) {
+  var exports = {};
+
+  var currentTextNodes = function() {
+    return document.evaluate(xpath, document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+  };
+
+  exports.apply = function(callback) {
+    var textNodes = currentTextNodes();
+    for (var i = 0; i < textNodes.snapshotLength; i++) {
+      var node = textNodes.snapshotItem(i);
+      node.data = callback(node.data)
+    }
+  };
+
+  return exports;
+};
+
+var mapper = TextMapper('/html/body/center/table/tbody//text()');
+mapper.apply(Beautifier.apply);
+
+// Doesn't work because it doesn't get parsed as HTML. :-(
+// mapper.apply(function(text) {
+//   return text.replace(/CTHULHU CULT/g, '<span style="font-varant: small-caps;">cthulhu cult</span>');
+// })
